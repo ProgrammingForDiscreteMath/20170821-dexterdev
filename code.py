88e21ced7f260d9251a472ca386e1da09d0750db
Devanand T <devanand.t.1986@gmail.com>
2017-08-23 09:51:48 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,17 @@
 # Create a list with the first ten triangular numbers
 # (see https://oeis.org/A000217)
 
-L = [ for i in range(10)]
+L=[i*(i+1)/2 for i in range(10)]
 
 # Create a function to test if a number is prime
 def is_prime(n):
     """
     Test if ``n`` is a prime.
     """
+    if n!=1:
+        return all(n % i for i in range(2, n))
+    else:
+        print "False"
 
 # Tests
 # is_prime(2033) == False
@@ -19,7 +23,6 @@ def is_prime(n):
 def next_ten_primes(n):
     """
     Return the list of the first ten prime numbers greate than or equal to n
-
     
     """
 
@@ -27,9 +30,3 @@ def next_ten_primes(n):
 # Tests
 # next_ten_primes(2033) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
 # next_ten_primes(2039) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
-
-
-
-
-
-
